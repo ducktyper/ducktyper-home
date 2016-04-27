b29509cd9309ab8024de292818709c9a7bcac9b5
Ducksan Cho <ducktyper@gmail.com>
2016-04-27 23:17:37 +1200

    Set title

--- ducktyper-angular2/src/app/components/project-list/project-list.ts
+++ ducktyper-angular2/src/app/components/project-list/project-list.ts
@@ -1,8 +1,15 @@
 import {Component} from 'angular2/core';
+import {Title} from 'angular2/platform/browser';
 
 @Component({
   selector: 'project',
   templateUrl: "app/components/project-list/project-list.html",
+  providers: [Title],
 })
 export class ProjectList {
+  constructor(public title:Title) {}
+
+  ngOnInit() {
+    this.title.setTitle("Ducktyper: Product List");
+  }
 }
