dfe1b5d7b73d9f99cdf02ae2e933bfff67d20c63
Ducksan Cho <ducktyper@gmail.com>
2016-04-30 09:01:30 +1200

    Do not deploy vendor and polyfills by default
    vendor and polyfills files are big and not change often.
    Ignore them on deploy by default helps to reduce upload time to S3.
    You can deploy them by run `npm run deploy-all`.

--- ducktyper-angular2/deploy/s3_upload.js
+++ ducktyper-angular2/deploy/s3_upload.js
@@ -1,15 +1,26 @@
 var fs = require('fs');
 var path = require('path');
 var AWS = require('aws-sdk'); 
+AWS.config.region = 'ap-southeast-2';
 
+// Load credential from ~/.aws/credential.js
+// process.env['AWS_ACCESS_KEY_ID']     = "XXX";
+// process.env['AWS_SECRET_ACCESS_KEY'] = "XXX";
 var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 require(path.join(home, ".aws", "credential.js"));
 
-
-AWS.config.region = 'ap-southeast-2';
+// config
+var ignoreVendor = true;
+process.argv.forEach(function (val, index, array) {
+  if (val === "--all") ignoreVendor = false;
+});
 
 // Deploy files under dist folder
 var dist = 'dist';
+var canIgnore = function(file) {
+  if (!ignoreVendor) return false;
+  return (file === "polyfills.bundle.js" || file === "vendor.bundle.js");
+}
 fs.readdir(dist, function (err, files) {
   if (err) {
     console.error( "Could not list the directory.", err);
@@ -17,7 +28,7 @@ fs.readdir(dist, function (err, files) {
   }
   files.forEach( function(file, index) {
     fs.stat(path.join(dist, file), function( error, stat ) {
-      if (stat.isFile()) {
+      if (stat.isFile() && !canIgnore(file)) {
         var filestream = fs.createReadStream(path.join(dist, file));
         var s3obj = new AWS.S3({params: {Bucket: 'ducktyper', Key: file}});
         s3obj.upload({Body: filestream}).
