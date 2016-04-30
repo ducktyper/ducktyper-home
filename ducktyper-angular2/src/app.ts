e293438880194862d730eb3e8cc2ec4fe356d07c
Ducksan Cho <ducktyper@gmail.com>
2016-04-30 12:23:43 +1200

    EnableProdMode in production

--- ducktyper-angular2/src/app.ts
+++ ducktyper-angular2/src/app.ts
@@ -2,10 +2,11 @@ import {bootstrap} from 'angular2/platform/browser';
 import {provide} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {enableProdMode} from 'angular2/core';
 
 import {DucktyperHome} from './app/ducktyper-home';
 
-
+if (process.env.NODE_ENV == 'production') enableProdMode();
 bootstrap(DucktyperHome, [
   HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
