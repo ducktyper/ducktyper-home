153012fafdd5d808511064a77f6cb103e975ef78
Ducksan Cho <ducktyper@gmail.com>
2016-04-30 12:53:42 +1200

    Use styleUrls

--- ducktyper-angular2/src/app/components/footer/footer.ts
+++ ducktyper-angular2/src/app/components/footer/footer.ts
@@ -4,7 +4,8 @@ var config = require('../../config/config');
 
 @Component({
   selector: 'footer',
-  templateUrl: `${config.assetHost}app/components/footer/footer.html`
+  templateUrl: `${config.assetHost}app/components/footer/footer.html`,
+  styleUrls: [`${config.assetHost}app/components/footer/footer.css`],
 })
 export class Footer {
 }
--- ducktyper-angular2/src/app/components/header/header.ts
+++ ducktyper-angular2/src/app/components/header/header.ts
@@ -5,6 +5,7 @@ var config = require('../../config/config');
 @Component({
   selector: 'header',
   templateUrl: `${config.assetHost}app/components/header/header.html`,
+  styleUrls: [`${config.assetHost}app/components/header/header.css`],
 })
 export class Header {
 }
