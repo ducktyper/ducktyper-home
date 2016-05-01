11fdfc973eae3eeceb71f0a506f1e1571a335842
Ducksan Cho <ducktyper@gmail.com>
2016-05-01 15:08:24 +1200

    Add database context for debug

---
+++ ducktyper-home/Models/DucktyperHomeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ducktyper_home.Models
+{
+    public class DucktyperHomeContext : DbContext
+    {
+        public DucktyperHomeContext() : base("DucktyperHomeContext")
+        {
+        }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
