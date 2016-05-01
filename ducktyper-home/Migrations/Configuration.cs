eb9cd20614985578d11b987ea88c5f8c05248e71
Ducksan Cho <ducktyper@gmail.com>
2016-05-01 15:10:06 +1200

    Enable code first migration
    > Enable-Migrations

---
+++ ducktyper-home/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace ducktyper_home.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ducktyper_home.Models.DucktyperHomeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ducktyper_home.Models.DucktyperHomeContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
