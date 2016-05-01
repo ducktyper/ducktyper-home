f9fafa7a79ed485d8b4017e49c5990ac468d40d3
Ducksan Cho <ducktyper@gmail.com>
2016-05-01 13:52:35 +1200

    Add api api/products

---
+++ ducktyper-home/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using ducktyper_home.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ducktyper_home.Controllers
+{
+    public class ProjectsController : ApiController
+    {
+        Project[] projects = new Project[]
+        {
+            new Project { Id = 1, Name = "Teaching Ruby", Details = "Learn ruby from building simple application", Link = "https://github.com/ducktyper/countdown" },
+            new Project { Id = 2, Name = "Let's Play", Details = "Playground for javascript and c#", Link = "https://github.com/ducktyper/letsplay" }
+        };
+
+        public IEnumerable<Project> GetAllProjects()
+        {
+            return projects.OrderByDescending(x => x.Id);
+        }
+    }
+}
