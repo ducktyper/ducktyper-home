d194c4aa7298d5253078c0637b5846b347631da2
Ducksan Cho <ducktyper@gmail.com>
2016-05-08 17:33:07 +1200

    Mock Http to pass test

--- ducktyper-angular2/src/app/project-repo.service.spec.ts
+++ ducktyper-angular2/src/app/project-repo.service.spec.ts
@@ -5,10 +5,26 @@ import {
   expect,
   inject
 } from '@angular/core/testing';
+import {MockBackend} from '@angular/http/testing';
+import {provide} from '@angular/core';
+import {
+  Http,
+  ConnectionBackend,
+  BaseRequestOptions,
+  Response,
+  ResponseOptions
+} from '@angular/http';
 import { ProjectRepoService } from './project-repo.service';
 
 describe('ProjectRepo Service', () => {
-  beforeEachProviders(() => [ProjectRepoService]);
+  beforeEachProviders(() => [
+    ProjectRepoService,
+    BaseRequestOptions,
+    MockBackend,
+    provide(Http, {useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+      return new Http(backend, defaultOptions);
+    }, deps: [MockBackend, BaseRequestOptions]}),
+  ]);
 
   it('should ...',
       inject([ProjectRepoService], (service: ProjectRepoService) => {
