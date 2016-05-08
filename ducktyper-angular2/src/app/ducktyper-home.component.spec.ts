2f2cd052c8b42fb0463ce774940c5232db06bf26
Ducksan Cho <ducktyper@gmail.com>
2016-05-08 17:57:30 +1200

    Moke Router to pass a test

--- ducktyper-angular2/src/app/ducktyper-home.component.spec.ts
+++ ducktyper-angular2/src/app/ducktyper-home.component.spec.ts
@@ -5,9 +5,29 @@ import {
   it,
   inject
 } from '@angular/core/testing';
+import {provide, ComponentResolver} from '@angular/core';
+import {
+  Router,
+  RouterOutletMap,
+  RouterUrlSerializer,
+  DefaultRouterUrlSerializer,
+} from '@angular/router';
+import {Location} from '@angular/common';
+import {SpyLocation} from '@angular/common/testing';
 import { DucktyperHomeAppComponent } from '../app/ducktyper-home.component';
 
-beforeEachProviders(() => [DucktyperHomeAppComponent]);
+beforeEachProviders(() => [
+  DucktyperHomeAppComponent,
+  RouterOutletMap,
+  provide(Location, {useClass: SpyLocation}),
+  provide(RouterUrlSerializer, {useClass: DefaultRouterUrlSerializer}),
+  provide(Router,
+    {
+      useFactory: (resolver, urlParser, outletMap, location) => new Router(
+        "RootComponent", DucktyperHomeAppComponent, resolver, urlParser, outletMap, location),
+      deps: [ComponentResolver, RouterUrlSerializer, RouterOutletMap, Location]
+    })
+]);
 
 describe('App: DucktyperHome', () => {
   it('should create the app',
