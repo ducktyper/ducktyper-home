1a56ac977924d6589907f24cf2a89677fdba3346
Ducksan Cho <ducktyper@gmail.com>
2016-05-08 18:20:09 +1200

    Mock Http and RouteSegment to pass tests

--- ducktyper-angular2/src/app/project-detail/project-detail.component.spec.ts
+++ ducktyper-angular2/src/app/project-detail/project-detail.component.spec.ts
@@ -9,12 +9,34 @@ import {
 import { ComponentFixture, TestComponentBuilder } from '@angular/compiler/testing';
 import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import {Title} from '@angular/platform-browser';
+import {RouteSegment, Router} from '@angular/router';
+import {MockBackend} from '@angular/http/testing';
+import {provide} from '@angular/core';
+import {
+  Http,
+  ConnectionBackend,
+  BaseRequestOptions,
+  Response,
+  ResponseOptions
+} from '@angular/http';
+import {ProjectRepoService} from '../project-repo.service';
 import { ProjectDetailComponent } from './project-detail.component';
 
 describe('Component: ProjectDetail', () => {
   let builder: TestComponentBuilder;
 
-  beforeEachProviders(() => [ProjectDetailComponent]);
+  beforeEachProviders(() => [
+    ProjectDetailComponent,
+    Title,
+    provide(RouteSegment, {useFactory: (r) => r.routeTree.root, deps: [Router]}),
+    ProjectRepoService,
+    BaseRequestOptions,
+    MockBackend,
+    provide(Http, {useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+      return new Http(backend, defaultOptions);
+    }, deps: [MockBackend, BaseRequestOptions]}),
+  ]);
   beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
     builder = tcb;
   }));
--- ducktyper-angular2/src/app/project-list/project-list.component.spec.ts
+++ ducktyper-angular2/src/app/project-list/project-list.component.spec.ts
@@ -9,17 +9,38 @@ import {
 import { ComponentFixture, TestComponentBuilder } from '@angular/compiler/testing';
 import { Component } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import {Title} from '@angular/platform-browser';
+import {MockBackend} from '@angular/http/testing';
+import {provide} from '@angular/core';
+import {
+  Http,
+  ConnectionBackend,
+  BaseRequestOptions,
+  Response,
+  ResponseOptions
+} from '@angular/http';
+import {ProjectRepoService} from '../project-repo.service';
 import { ProjectListComponent } from './project-list.component';
+
 
 describe('Component: ProjectList', () => {
   let builder: TestComponentBuilder;
 
-  beforeEachProviders(() => [ProjectListComponent]);
+  beforeEachProviders(() => [
+    ProjectListComponent,
+    Title,
+    ProjectRepoService,
+    BaseRequestOptions,
+    MockBackend,
+    provide(Http, {useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+      return new Http(backend, defaultOptions);
+    }, deps: [MockBackend, BaseRequestOptions]}),
+  ]);
   beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
     builder = tcb;
   }));
 
-  it('should inject the component', inject([ProjectListComponent],
+  it('should inject the component', inject([ProjectListComponent, Title],
       (component: ProjectListComponent) => {
     expect(component).toBeTruthy();
   }));
